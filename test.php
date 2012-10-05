b3b3a381edf977c1cef99a364992a691bc8cc6cf
Victor Antoine <contact@victor-antoine.fr>
2012-10-05 14:51:32 +0200

    Mauvais nom de table

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 include 'classes/connexion.class.php';
 $connexion = new Connexion("mysql", "localhost", "2012_vantoine", "2012_vantoine", "jpkc5k6t");
-$array = $connexion->query("SELECT * FROM test");
+$array = $connexion->query("SELECT * FROM Test");
 print_r($array);
 ?>
