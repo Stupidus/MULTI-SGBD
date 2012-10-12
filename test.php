bacfb6be5d65e6539e9580c14c939de5bce49aa6
Victor Antoine <contact@victor-antoine.fr>
2012-10-12 13:30:09 +0200

    supression fichier de test

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'classes/connexion.class.php';
-$connexion = new Connexion("mysql", "localhost", "2012_vantoine", "2012_vantoine", "jpkc5k6t");
-$array = $connexion->query("SELECT * FROM Test");
-print_r($array);
-?>
