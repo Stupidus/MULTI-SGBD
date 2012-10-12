7e1c245267a502a09b41102b11ca5b0208dea0e7
Victor Antoine <contact@victor-antoine.fr>
2012-10-12 15:19:15 +0200

    Passage variables connexion

--- process_connexion.php
+++ process_connexion.php
@@ -2,7 +2,14 @@
 try {
     echo "<h3>Connexion en cours</h3>";
     $connexion = new Connexion($_POST['sgbd'], $_POST['host'], $_POST['dbname'], $_POST['username'], $_POST['password'], $_POST['port']);
-    $_SESSION['connexion'] = serialize($connexion);
+    $_SESSION['connexion'] = array(
+        "SGBD" => $_POST['sgbd'],
+        "host" => $_POST['host'],
+        "dbname" => $_POST['dbname'],
+        "username" => $_POST['username'],
+        "password" => $_POST['password'],
+        "port" => $_POST['port']
+    );
     echo "Vous êtes bien connecté à la base de données : <a href='?cat=2'>Continuer</a>";
 }
 catch(Exception $e)
