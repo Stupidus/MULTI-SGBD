2a7407c4ab881e721872d0d050c30e2b40906c99
Victor Antoine <contact@victor-antoine.fr>
2012-10-12 14:49:10 +0200

    Redirection après connexion

--- process_connexion.php
+++ process_connexion.php
@@ -1,6 +1,9 @@
 <?php
 try {
+    echo "<h3>Connexion en cours</h3>";
     $connexion = new Connexion($_POST['sgbd'], $_POST['host'], $_POST['dbname'], $_POST['username'], $_POST['password'], $_POST['port']);
+    $_SESSION['connexion'] = $connexion;
+    echo "Vous êtes bien connecté à la base de données : <a href='?cat=2'>Continuer</a>";
 }
 catch(Exception $e)
 {
