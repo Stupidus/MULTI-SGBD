e916aa315984694ea3e75d18540bcf75db3c30c0
Victor Antoine <contact@victor-antoine.fr>
2012-10-26 14:15:55 +0200

    Ajout formulaire connexion

---
+++ connexion_appli.php
@@ -0,0 +1,46 @@
+<h3>Connexion</h3>
+<?php
+    if(isset($_GET['m']) && $_GET['m'] == "1")        
+    {
+        try
+        {
+            $connexion = new Connexion("oracle", "grive2.u-strasbg.fr", "LPDT", "TASTYSGBD", "tastypassword", "1591");
+            $q = $connexion->query("SELECT * FROM USERS WHERE username = :username && password = :password", array(":username" => $_POST['username'], ":password" => $_POST['username']));
+            if(sizeof($q) > 0)
+            {
+                $_SESSION['auth'] = true;
+                $_SESSION['id_user'] = $q[0]['id'];
+                echo "Vous avez été correctement identifié par l'application.";
+            }
+            else
+            {
+                $_SESSION['auth'] = false;
+            }
+        }
+        catch(Exception $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+    else
+    {
+        ?>
+        <div id="center">
+        <fieldset>
+                <h3>Connexion à l'application</h3>
+                <form action="?m=1" method="POST">
+                        <label for="username">Utilisateur : </label>                        
+                        <input type="text" name="username" id="username"/>
+                        <br/>
+                        <label for="password">Password : </label>
+                        <input type="text" name="password" id="password"/>
+                        <br/>                        
+                        <div id="centerbutton">
+                        <input type="submit" value="Connexion"/>
+                        </div>
+                </form>
+        </fieldset>
+        </div>
+        <?php
+    }    
+?>
