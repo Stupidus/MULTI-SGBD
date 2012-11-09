433d60033afdac8690461ea89b4fffb9982f0e50
Victor Antoine <contact@victor-antoine.fr>
2012-11-09 14:27:21 +0100

    ordre des variables

--- process_connexion.php
+++ process_connexion.php
@@ -10,7 +10,7 @@ switch($m)
     default:
         $listeConnexions = $connexion->query("SELECT * FROM CONNEXIONS WHERE ID = :ID", array(":ID" => $_GET['connexion_id']));
         try {
-            $connexionTest = new Connexion($listeConnexions[0]['SGBD'], $listeConnexions[0]['HOST'], $listeConnexions[0]['PORT'], $listeConnexions[0]['DBNAME'], $listeConnexions[0]['USERNAME'], $listeConnexions[0]['PASSWORD']);
+            $connexionTest = new Connexion($listeConnexions[0]['SGBD'], $listeConnexions[0]['HOST'], $listeConnexions[0]['DBNAME'], $listeConnexions[0]['USERNAME'], $listeConnexions[0]['PASSWORD'], $listeConnexions[0]['PORT']);
             $_SESSION['id_connexion'] = $_GET['id_connexion'];
             echo "La connexion ".$listeConnexions[0]['LABEL']." a bien été selectionnée. <a href='?cat=2'>Retour</a>";
         }
