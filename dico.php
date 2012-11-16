918ead60677affb0c4003cb5205b8d594ed307a9
Victor Antoine <contact@victor-antoine.fr>
2012-11-16 14:09:55 +0100

    pb mysql

--- dico.php
+++ dico.php
@@ -5,6 +5,7 @@ if($connexion->getSgbd() == "oracle")
     $dictionnaire = $connexion_bdd->query("SELECT TABLE_NAME FROM DICT");
 else if($connexion->getSgbd() == "mysql")
     $dictionnaire = $connexion_bdd->query("SELECT TABLE_NAME FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA` IN ('information_schema', 'mysql') ORDER BY `TABLE_NAME`");
+var_dump($dictionnaire);
 ?>
 <table style="border:1px solid black;">
     <thead>
