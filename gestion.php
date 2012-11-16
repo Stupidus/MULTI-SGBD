0d53a1561477eba833abf3ca8d04bfe187348309
Victor Antoine <contact@victor-antoine.fr>
2012-11-16 13:38:32 +0100

    Panrl gestion bdd

--- gestion.php
+++ gestion.php
@@ -1,3 +1,5 @@
-<?php
-
-?>
+<a href="?cat=5">Afficher le dictionnaire de données</a>
+<br/><br/>
+<a href="?cat=6">Script de création des tables d'un utilisteur</a>
+<br/><br/>
+<a href="?cat=7">Sauvegarde de la base de données</a>
